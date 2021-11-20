f2ac5886d9f78ae04b85678708cc37af4eae1e94
Petro Dudi <pdudis@gmail.com>
2021-11-20 22:25:36 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace $safeprojectname$
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Template 1", 0);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Template 2", 1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Template 3", 2);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("Template 4", 3);
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Template 5", 4);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("Template 6", 5);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.copyToClipboard = new System.Windows.Forms.Button();
+            this.about = new System.Windows.Forms.Button();
+            this.quit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Template 1 - preview.png");
+            this.imageList1.Images.SetKeyName(1, "Template 2 - preview.png");
+            this.imageList1.Images.SetKeyName(2, "Template 3 - preview.png");
+            this.imageList1.Images.SetKeyName(3, "Template 4 - preview.png");
+            this.imageList1.Images.SetKeyName(4, "Template 5 - preview.png");
+            this.imageList1.Images.SetKeyName(5, "Template 6 - preview.png");
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(862, 126);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 144);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1040, 574);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // copyToClipboard
+            // 
+            this.copyToClipboard.Location = new System.Drawing.Point(884, 12);
+            this.copyToClipboard.Name = "copyToClipboard";
+            this.copyToClipboard.Size = new System.Drawing.Size(168, 38);
+            this.copyToClipboard.TabIndex = 2;
+            this.copyToClipboard.Text = "Copy to Clipboard";
+            this.copyToClipboard.UseVisualStyleBackColor = true;
+            this.copyToClipboard.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // about
+            // 
+            this.about.Location = new System.Drawing.Point(884, 56);
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(168, 38);
+            this.about.TabIndex = 3;
+            this.about.Text = "About";
+            this.about.UseVisualStyleBackColor = true;
+            this.about.Click += new System.EventHandler(this.about_Click);
+            // 
+            // quit
+            // 
+            this.quit.Location = new System.Drawing.Point(884, 100);
+            this.quit.Name = "quit";
+            this.quit.Size = new System.Drawing.Size(168, 38);
+            this.quit.TabIndex = 4;
+            this.quit.Text = "Quit";
+            this.quit.UseVisualStyleBackColor = true;
+            this.quit.Click += new System.EventHandler(this.quit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 729);
+            this.Controls.Add(this.quit);
+            this.Controls.Add(this.about);
+            this.Controls.Add(this.copyToClipboard);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "$safeprojectname$";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button copyToClipboard;
+        private System.Windows.Forms.Button about;
+        private System.Windows.Forms.Button quit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace $safeprojectname$
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.DocumentText = Properties.Resources.Intro;
+        }
+
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           if (listView1.Items[0].Selected)
+            {
+                webBrowser1.DocumentText = Properties.Resources.Template_1_HTML;
+            } else if (listView1.Items[1].Selected)
+            {
+                webBrowser1.DocumentText = Properties.Resources.Template_2_HTML;
+            } else if (listView1.Items[2].Selected)
+            {
+                webBrowser1.DocumentText = Properties.Resources.Template_3_HTML;
+            } else if (listView1.Items[3].Selected)
+            {
+                webBrowser1.DocumentText = Properties.Resources.Template_4_HTML;
+            } else if (listView1.Items[4].Selected)
+            {
+                webBrowser1.DocumentText = Properties.Resources.Template_5_HTML;
+            } else if (listView1.Items[5].Selected)
+            {
+                webBrowser1.DocumentText = Properties.Resources.Template_6_HTML;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Clipboard.SetDataObject(webBrowser1.Document.Body.InnerHtml);
+
+        }
+
+        private void quit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void about_Click(object sender, EventArgs e)
+        {
+           
+            webBrowser1.DocumentText = Properties.Resources.Intro;
+        }
+    }
+
+    public static class MyGlobals
+    {
+        public static string webpageText;
+    }
+
+
+}
